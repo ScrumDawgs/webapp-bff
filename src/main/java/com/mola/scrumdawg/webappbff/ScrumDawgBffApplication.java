53acc144fae916c32bd446dea2ac45c67f62f6ef
ScrumDawg <ma.abdelmola@gmail.com>
2024-02-04 18:21:49 +0100

    Initial Commit

---
+++ src/main/java/com/mola/scrumdawg/webappbff/ScrumDawgBffApplication.java
@@ -0,0 +1,13 @@
+package com.mola.scrumdawg.webappbff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrumDawgBffApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScrumDawgBffApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mola/scrumdawg/webappbff/ScrumDawgBffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mola.scrumdawg.webappbff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrumDawgBffApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
